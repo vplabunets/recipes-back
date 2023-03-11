b640a6c757679ebcb48de245d81043c48031be1e
Volodymyr Labunets <vplabunets@gmail.com>
2023-03-11 21:36:22 +0200

    add server

--- server.js
+++ server.js
@@ -7,14 +7,21 @@ const { app } = require("./app");
 mongoose.set("strictQuery", false);
 
 const { HOST_URI } = process.env;
-const PORT = process.env.PORT || 3001;
-console.log(process.env);
+const PORT = process.env.PORT || 3000;
+// console.log(process.env);
 async function main() {
   try {
     await mongoose.connect(HOST_URI);
     console.log("Connected to mongodb");
     app.listen(PORT, () => {
       console.log(`server is listening on port ${PORT}`);
+    });
+    app.get("/", (req, res) => {
+      res.send("Hey this is my API running 🥳");
+    });
+
+    app.get("/about", (req, res) => {
+      res.send("This is my about route..... ");
     });
   } catch (error) {
     console.error("Main failed", error.message);
