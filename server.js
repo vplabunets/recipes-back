1ac88560817520a5b9d5ce42f21f08c5b548c218
Volodymyr Labunets <vplabunets@gmail.com>
2023-03-13 15:58:36 +0200

    fix server

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const { app } = require("./app");
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
-const xxx= process.env.HOST_URI.toString();
+// const xxx= process.env.HOST_URI.toString();
+const xxx = "mongodb+srv://admin:H8r7RG7jUWF6gfiO@recipes.gpibk5y.mongodb.net/recipesDB?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 3000;
 
   try {
     await console.log("HOST_URI in main in try", HOST_URI);
-
     await mongoose.connect(xxx);
     console.log("Connected to mongodb");
     await app.listen(PORT, () => {
