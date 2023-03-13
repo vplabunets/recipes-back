0be73ea54b66f13a4e8797b49e75e8969e279618
Volodymyr Labunets <vplabunets@gmail.com>
2023-03-13 15:31:41 +0200

    fix uri

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const { HOST_URI } = process.env;
 const PORT = process.env.PORT || 3000;
 
 async function main() {
-  console.log(HOST_URI);
+  console.log("HOST_URI in main before try", HOST_URI);
   try {
+    console.log("HOST_URI in main in try", HOST_URI);
+
     await mongoose.connect(HOST_URI);
     console.log("Connected to mongodb");
     await app.listen(PORT, () => {
