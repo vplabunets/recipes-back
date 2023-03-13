bc8d8e890ba107cea8ba98efa3bc0658d5955f74
Volodymyr Labunets <vplabunets@gmail.com>
2023-03-13 16:20:41 +0200

    mongo

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-require("dotenv").config();
+require("dotenv").config({ path: "./config.env" });
 
 const { app } = require("./app");
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
-const { HOST_URI } = process.env;
-const PORT = process.env.PORT || 3000;
+// const { HOST_URI } = process.env;
+// const PORT = process.env.PORT || 3000;
 
-const xxx =
-  "mongodb+srv://admin:H8r7RG7jUWF6gfiO@recipes.gpibk5y.mongodb.net/recipesDB?retryWrites=true&w=majority";
+const DB = process.env.HOST_URI;
+// const Port = process.env.PORT;
+
+// const xxx =
+//   "mongodb+srv://admin:H8r7RG7jUWF6gfiO@recipes.gpibk5y.mongodb.net/recipesDB?retryWrites=true&w=majority";
 async function main() {
   await console.log("HOST_URI in main before try", HOST_URI);
   try {
     await console.log("HOST_URI in main in try", HOST_URI);
 
-    await mongoose.connect(xxx);
+    await mongoose.connect(DB);
     console.log("Connected to mongodb");
     await app.listen(PORT, () => {
       console.log(`server is listening on port ${PORT}`);
